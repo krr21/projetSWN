946110d1c2ea9ccaf0a39d7a45c7afc0ccd73bf9
Unknown <mgrkrr21@gmail.com>
2018-11-15 11:18:42 +0100

    new file

---
+++ app/src/main/java/com/guy/hp/snow/Activity/Main2Activity.java
@@ -0,0 +1,15 @@
+package com.guy.hp.snow.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.guy.hp.snow.R;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
